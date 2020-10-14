804c9baa0835b24aa9984b6206654880a9ca2573
Farah-Ali <farah.ali9805@gmail.com>
2020-10-14 16:00:16 +0300

    step1

---
+++ safe.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form >
+  <label for="fname">Username</label><br>
+  <inputtype="text" id="username" name="fname"><br>
+  <label for="lname">Password</label>
+  <input type="text" id="lname" name="lname"><br><br>
+
+  </form>
+
+
+</body>
+</html>
